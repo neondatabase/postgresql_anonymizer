34250fa6d1453252faba520e3472791ff8f46fd9
damien clochard <damien@dalibo.com>
2018-08-03 09:24:58 +0200

    [unit] fixup data test

--- tests/unit.sql
+++ tests/unit.sql
@@ -25,5 +25,5 @@ SELECT pg_typeof(anon.random_company());
 -- Date
 --
 SELECT pg_typeof(anon.random_date_between('01/01/1900'::TIMESTAMP WITH TIME ZONE,now()));
-SELECT pg_typeof(anon.random_date_between('01/01/0000'::DATE,'O1/O1/4000'::DATE);
+SELECT pg_typeof(anon.random_date_between('01/01/0000'::DATE,'O1/O1/4000'::DATE));
 SELECT pg_typeof(anon.random_date());
