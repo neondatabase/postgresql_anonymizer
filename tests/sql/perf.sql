2c2952124bdb2a582c88c0a8e1d9c57d77a9ab21
damien clochard <damien@dalibo.com>
2018-08-18 09:36:49 +0200

    [perf] simple test

---
+++ tests/sql/perf.sql
@@ -0,0 +1,37 @@
+CREATE EXTENSION IF NOT EXISTS anon;
+
+--SET search_path TO pg_temp; 
+
+CREATE TABLE customer(
+	id SERIAL,
+	full_name TEXT
+);
+
+INSERT INTO customer(id, full_name)
+SELECT generate_series(1,1000), 'X Y';
+
+
+\timing
+
+-- basic modification
+UPDATE customer SET full_name='A B';
+
+
+UPDATE customer                                                                                                                                        
+SET                                                                                                                                                    
+    full_name=anon.random_first_name()                                                                              
+;      
+
+UPDATE customer                                                                                                                                        
+SET                                                                                                                                                    
+    full_name=anon.random_last_name()                                                                              
+;      
+
+UPDATE customer
+SET 
+	full_name=anon.random_first_name() || ' ' || anon.random_last_name(),
+;
+
+
+
+
