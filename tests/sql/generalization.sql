5655110d451856dfb6a4a477fc3f10b929095270
damien clochard <damien@dalibo.com>
2019-10-19 15:02:41 +0200

    [generalization] fix tsrange test

--- tests/sql/generalization.sql
+++ tests/sql/generalization.sql
@@ -17,25 +17,26 @@ SELECT anon.generalize_int8range(NULL,10000000000);
 SELECT anon.generalize_int8range(NULL,NULL);
 
 -- generalize_numrange
-SELECT generalize_numrange(0.36683);
-SELECT generalize_numrange(0.32378,0.01);
-SELECT generalize_numrange(NULL);
-SELECT generalize_numrange(NULL,0.001);
-SELECT generalize_numrange(NULL,NULL);
+SELECT anon.generalize_numrange(0.36683);
+SELECT anon.generalize_numrange(0.32378,0.01);
+SELECT anon.generalize_numrange(NULL);
+SELECT anon.generalize_numrange(NULL,0.001);
+SELECT anon.generalize_numrange(NULL,NULL);
 
-SELECT generalize_tsrange('19041107');
-SELECT generalize_tsrange('19041107','microsecond');
-SELECT generalize_tsrange('19041107','millisecond');
-SELECT generalize_tsrange('19041107','second');
-SELECT generalize_tsrange('19041107','minute');
-SELECT generalize_tsrange('19041107','hour');
-SELECT generalize_tsrange('19041107','day');
-SELECT generalize_tsrange('19041107','week');
-SELECT generalize_tsrange('19041107','month');
-SELECT generalize_tsrange('19041107','quarter');
-SELECT generalize_tsrange('19041107','year');
-SELECT generalize_tsrange('19041107','decade');
-SELECT generalize_tsrange('19041107','century');
-SELECT generalize_tsrange('19041107','millennium');
+SELECT anon.generalize_tsrange('19041107');
+SELECT anon.generalize_tsrange(NULL);
+SELECT anon.generalize_tsrange('19041107','microsecond');
+SELECT anon.generalize_tsrange('19041107','millisecond');
+SELECT anon.generalize_tsrange('19041107','second');
+SELECT anon.generalize_tsrange('19041107','minute');
+SELECT anon.generalize_tsrange('19041107','hour');
+SELECT anon.generalize_tsrange('19041107','day');
+SELECT anon.generalize_tsrange('19041107','week');
+SELECT anon.generalize_tsrange('19041107','month');
+SELECT anon.generalize_tsrange('19041107','quarter');
+SELECT anon.generalize_tsrange('19041107','year');
+SELECT anon.generalize_tsrange('19041107','decade');
+SELECT anon.generalize_tsrange('19041107','century');
+SELECT anon.generalize_tsrange('19041107','millennium');
 
 ROLLBACK;
