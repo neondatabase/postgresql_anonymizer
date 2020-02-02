52771f9ab179f21cd59134f95f40cf7b40c681a4
Damien Clochard <damien@dalibo.com>
2020-02-02 15:13:14 +0100

    [detect] init test

---
+++ tests/sql/detection.sql
@@ -0,0 +1,29 @@
+
+BEGIN;
+
+CREATE EXTENSION IF NOT EXISTS anon CASCADE;
+
+-- INIT
+
+SELECT anon.load();
+
+
+CREATE TABLE customer (
+  id SERIAL,
+  firstname TEXT,
+  last_name TEXT,
+  "CreditCard" TEXT
+);
+
+CREATE TABLE vendor (
+  employee_id INTEGER UNIQUE,
+  "Firstname" TEXT,
+  lastname TEXT,
+  phone_number TEXT,
+  birth DATE
+);
+
+
+SELECT anon.detect();
+
+ROLLBACK;
