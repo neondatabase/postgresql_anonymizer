90b30fb98f9670e976a2c79ced67b6aab0571539
damien clochard <damien@dalibo.info>
2020-02-08 10:02:34 +0000

    Resolve "invalid command \."

--- anon.sql
+++ anon.sql
@@ -1259,11 +1259,20 @@ CREATE OR REPLACE FUNCTION @extschema@.get_copy_statement(relid OID)
 RETURNS TEXT AS
 $$
 DECLARE
+  empty_table BOOLEAN;
   copy_statement TEXT;
   val TEXT;
   rec RECORD;
 BEGIN
---  /!\ cannot use COPY TO STDOUT in PL/pgSQL
+-- Stop right now if the table is empty
+  EXECUTE format(E'SELECT true WHERE NOT EXISTS (SELECT 1 FROM %s);',
+                                                    relid::REGCLASS)
+  INTO empty_table;
+  IF empty_table THEN
+    RETURN '';
+  END IF;
+
+  --  /!\ cannot use COPY TO STDOUT in PL/pgSQL
   copy_statement := format(E' COPY %s
                               FROM STDIN
                               CSV QUOTE AS ''"''
