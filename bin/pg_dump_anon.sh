9f60f57e533c9809f85c46be1e9c3d79d6ee05f7
damien clochard <damien@dalibo.com>
2020-02-12 21:34:40 +0100

    [pg_dump_anon] improve checks

--- bin/pg_dump_anon.sh
+++ bin/pg_dump_anon.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
+#
+#    pg_dump_anon
+#    A basic wrapper to export anonymized data with pg_dump and psql
+#
+#    This is work in progress. Use with care.
+#
+#
 
+usage()
+{
+  echo "Usage: $(basename $0) [OPTION]... [DBNAME]"
+  echo
+  echo "Connection options:"
+  echo
+  echo "-d, --dbname=DBNAME      database to dump"
+  echo "-h, --host=HOSTNAME      database server host or socket directory"
+  echo "-p, --port=PORT          database server port number"
+  echo "-U, --username=NAME      connect as specified database user"
+  echo "-w, --no-password        never prompt for password"
+  echo "-W, --password           force password prompt (should happen automatically)"
+  echo "--role=ROLENAME          do SET ROLE before dump"
+}
 
-# TODO intercept the --help arg
 
 # TODO filter out the pg_dump args that are no supported by psql
 PSQL="psql $@ --quiet --tuples-only --no-align"
 
-# TODO stop if the extension is not installed in the database
+get_anon_version() {
+$PSQL << EOSQL
+  SELECT extversion FROM pg_catalog.pg_extension WHERE extname='anon';
+EOSQL
+}
 
 ## Return the table name based on the relid
 get_table_name() {
@@ -17,7 +41,7 @@ EOSQL
 }
 
 ## Return the masking filters based on the relid
-get_mask_filters(){
+get_mask_filters() {
 $PSQL << EOSQL
   SELECT anon.mask_filters($1);
 EOSQL
@@ -43,6 +67,33 @@ $PSQL << EOSQL
   ORDER BY  relid::regclass -- sort by name to force the dump order
 EOSQL
 }
+
+##
+## M A I N
+##
+
+## Checks options
+options=$(getopt -o ? --long help -- "$@")
+eval set -- "$options"
+#while true; do
+#    case "$1" in
+#    --help)
+#        usage
+#        exit 0
+#        ;;
+#    esac
+#    shift
+#done
+
+## Stop if the extension is not installed in the database
+version=$(get_anon_version)
+if [ -z "$version" ]
+then
+  echo 'Anon extension is not installed in this database.'
+  exit 1
+fi
+
+## Header
 
 ## Dump the DDL
 pg_dump --schema-only "$@"
