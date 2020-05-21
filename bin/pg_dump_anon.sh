8589355334770573b4ae68bdb7c5e9ffa17cd011
Joe Auty <jauty@flipgive.com>
2020-05-21 17:54:55 -0400

    reset sequences

--- bin/pg_dump_anon.sh
+++ bin/pg_dump_anon.sh
@@ -226,3 +226,25 @@ do
   echo
 done
 
+reset_sequences=$(cat << EOF
+SELECT 'SELECT SETVAL(' ||
+       quote_literal(quote_ident(PGT.schemaname) || '.' || quote_ident(S.relname)) ||
+       ', COALESCE(MAX(' ||quote_ident(C.attname)|| '), 1) ) FROM ' ||
+       quote_ident(PGT.schemaname)|| '.'||quote_ident(T.relname)|| ';'
+FROM pg_class AS S,
+     pg_depend AS D,
+     pg_class AS T,
+     pg_attribute AS C,
+     pg_tables AS PGT
+WHERE S.relkind = 'S'
+    AND S.oid = D.objid
+    AND D.refobjid = T.oid
+    AND D.refobjid = C.attrelid
+    AND D.refobjsubid = C.attnum
+    AND T.relname = PGT.tablename
+ORDER BY S.relname;
+EOF
+)
+
+reset_sequences_sql=`$PSQL_PRINT -c "$reset_sequences"`
+echo "$reset_sequences_sql"
