030e88c79c03e343040e58ece2a689daaa95eb22
damien clochard <damien@dalibo.com>
2020-05-23 09:00:18 +0200

    [lint] improve mdl config

--- .mdstyle.rb
+++ .mdstyle.rb
@@ -1,4 +1,9 @@
 all
 rule 'MD003', :style => :setext_with_atx
+exclude_rule 'MD004'
 exclude_rule 'MD012'
+rule "MD024", :allow_different_nesting => true
+exclude_rule 'MD026'
+exclude_rule 'MD034'
 exclude_rule 'MD041'
+rule 'MD046', :style => :consistent
