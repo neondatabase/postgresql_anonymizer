c87f926a6551ed8d189d2f2f24d0689aa1d8d5a0
damien clochard <damien@dalibo.com>
2020-09-28 10:34:09 +0200

    [lint] fixup markdown header rule

--- .mdstyle.rb
+++ .mdstyle.rb
@@ -3,6 +3,7 @@ rule 'MD003', :style => :setext_with_atx
 exclude_rule 'MD004'
 exclude_rule 'MD012'
 rule "MD024", :allow_different_nesting => true
+rule 'MD025', :level => 2
 exclude_rule 'MD026'
 exclude_rule 'MD034'
 exclude_rule 'MD041'
