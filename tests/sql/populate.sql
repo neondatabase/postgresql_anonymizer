4556cead0868536ebf06954fc192fe0969efce71
damien clochard <damien@dalibo.com>
2021-05-16 16:45:15 +0200

    [tests] fixup populate

--- tests/sql/populate.sql
+++ tests/sql/populate.sql
@@ -8,7 +8,7 @@ TRUNCATE anon.email;
 
 COPY anon.email
   FROM PROGRAM
-  'python3 $(pg_config --sharedir)/extension/anon/populate.py --table email --lines 500';
+  '/usr/bin/python3 $(pg_config --sharedir)/extension/anon/populate.py --table email --lines 500';
 
 SELECT count(DISTINCT val)=500 FROM anon.email;
 
