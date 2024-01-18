39fe9e27234c43b7f404c959c015b02fcde100ff
damien clochard <damien@dalibo.com>
2024-01-18 17:16:36 +0100

    [core] Add a compat module

---
+++ src/compat.rs
@@ -0,0 +1,45 @@
+///
+/// # Custom compatibility types and functions
+///
+/// Each major version of Postgres may introduce internal API changes.
+///
+/// We're declaring here a set of replacements to avoid conditional macro
+/// codeblocks in the main extension codebase.
+///
+
+use pgrx::prelude::*;
+
+///
+/// rawparser function
+///
+
+#[cfg(any(feature = "pg12", feature = "pg13"))]
+pub unsafe fn raw_parser(query: *const i8) -> *mut pg_sys::List {
+    pg_sys::raw_parser(query)
+}
+
+#[cfg(any(feature = "pg14", feature = "pg15", feature = "pg16"))]
+pub unsafe fn raw_parser(query: *const i8) -> *mut pg_sys::List {
+    pg_sys::raw_parser(query,pg_sys::RawParseMode_RAW_PARSE_DEFAULT)
+}
+
+///
+/// SchemaValue type
+///
+
+#[cfg(any(feature = "pg15", feature = "pg16"))]
+pub use pgrx::pg_sys::String as SchemaValue;
+
+#[cfg(any(feature = "pg11", feature = "pg12", feature = "pg13", feature = "pg14"))]
+pub use pgrx::pg_sys::Value as SchemaValue;
+
+///
+/// strVal macro
+///
+
+#[cfg(any(feature = "pg15", feature = "pg16"))]
+pub unsafe fn strVal(v: SchemaValue) -> *const i8 { v.sval }
+
+#[allow(non_snake_case)]
+#[cfg(any(feature = "pg11", feature = "pg12", feature = "pg13", feature = "pg14"))]
+pub unsafe fn strVal(v: SchemaValue) -> *const i8 { v.val.str_ }
--- src/lib.rs
+++ src/lib.rs
@@ -276,6 +276,82 @@ unsafe extern "C" fn pa_masking_policy_object_relabel(
 // Internal Functions
 //----------------------------------------------------------------------------
 
+
+/// check that an expression is a valid masking function
+///
+/// The function does not return false, but throws a Postgres error if a value
+/// expression is incorrect
+///
+fn pa_check_function( expr: &str) -> bool {
+    use std::ptr;
+
+    let func = pa_parse_expression(expr).expect("expression should be valid");
+    debug1!("Anon: value.type_ = {:#?}", func.type_ );
+    if ! unsafe {
+        pgrx::is_a(func.as_ptr(),pg_sys::NodeTag::T_FuncCall)
+    } {
+        ereport!(
+            ERROR,
+            ERRCODE_SYNTAX_ERROR,
+            format!("{expr} is not a correct masking function")
+        );
+    }
+
+    // Walk through the parse tree and the function itself and all other
+    // functions used as parameters belong to a trusted schema.
+    // The goal is to block privilege escalation attacks using something like:
+    //
+    // `MASKED WITH FUNCTION pg_catalog.upper(public.elevate())`
+    //
+
+    ! pa_has_untrusted_schema(func.as_ptr(),ptr::null_mut())
+
+}
+
+#[pg_guard]
+extern "C" fn pa_has_untrusted_schema(   node: *mut pg_sys::Node,
+                        context: *mut ::core::ffi::c_void
+) -> bool {
+
+    if node.is_null() { return false ; }
+
+    if unsafe {
+        pgrx::is_a(node,pg_sys::NodeTag::T_FuncCall)
+    } {
+        let fc = unsafe {
+            PgBox::from_pg(node as *mut pg_sys::FuncCall)
+        };
+        // fc.funcname is a pointer to a pg_sys::List
+        let funcname = unsafe {
+            PgBox::from_pg(fc.funcname)
+        };
+
+        if funcname.length == 2 {
+            // the function name is qualified, the first element of the list
+            // is the schema name
+            let schema_val = unsafe {
+                PgBox::from_pg(
+                    pg_sys::pgrx_list_nth(funcname.as_ptr(), 0)
+                    as *mut crate::compat::SchemaValue
+                )
+            };
+            let schema_c_ptr=unsafe{crate::compat::strVal(*schema_val)};
+            let schema_c_str = unsafe {CStr::from_ptr(schema_c_ptr)};
+            let schema_str=schema_c_str.to_str().unwrap();
+            if schema_str!="anon" {
+                return true;
+            }
+        }
+    }
+    unsafe {
+        pg_sys::raw_expression_tree_walker( node,
+                                        Some(pa_has_untrusted_schema),
+                                        context)
+    }
+}
+
+
+
 /// Prepare a Raw Statement object that will replace the authentic relation
 ///
 /// * relid is the oid of the relation
@@ -445,6 +521,65 @@ fn pa_masking_value_for_att(
     "NULL".to_string()
 }
 
+// Parse a given expression and return its raw statement
+//
+fn pa_parse_expression(expr: &str) -> Option<PgBox<pg_sys::Node>>
+{
+    if expr.is_empty() {
+        ereport!(ERROR,
+                 ERRCODE_NULL_VALUE_NOT_ALLOWED,
+                 format!("expression is empty")
+        );
+    }
+
+    let query_string = format!("SELECT {expr}");
+    let query_c_string = CString::new(query_string.as_str()).unwrap();
+    let raw_parsetree_list = unsafe {
+        crate::compat::raw_parser(
+            query_c_string.as_c_str().as_ptr()
+            as *const pgrx::ffi::c_char
+        )
+    };
+
+    // Only one statement in the parsetree is allowed
+    if unsafe { raw_parsetree_list.as_ref().unwrap().length > 1 } {
+        ereport!(ERROR,
+                 ERRCODE_SYNTAX_ERROR,
+                 format!("expression is not correct")
+        );
+    }
+    let raw_stmt = unsafe {
+        // this is the equivalent of the linitial_node C macro
+        // https://doxygen.postgresql.org/pg__list_8h.html#a213ac28ac83471f2a47d4e3918f720b4
+        PgBox::from_pg(
+            pg_sys::pgrx_list_nth(raw_parsetree_list, 0)
+            as *mut pg_sys::RawStmt
+        )
+    };
+
+    let stmt = unsafe {
+        PgBox::from_pg( raw_stmt.stmt as *mut pg_sys::SelectStmt )
+    };
+
+    // Only one expression in the target is allowed
+    if unsafe { stmt.targetList.as_ref().unwrap().length > 1 } {
+        ereport!(ERROR,
+                 ERRCODE_SYNTAX_ERROR,
+                 format!("expression is not correct")
+        );
+    }
+
+    let restarget = unsafe {
+        PgBox::from_pg(
+            pg_sys::pgrx_list_nth(stmt.targetList, 0)
+            as *mut pg_sys::ResTarget
+        )
+    };
+
+    Some(unsafe { PgBox::from_pg( restarget.val as *mut pg_sys::Node ) })
+}
+
+
 /// Return the quoted name of a NameData identifier
 /// if a column is named `I`, its quoted name is `"I"`
 ///
@@ -561,6 +696,11 @@ mod anon {
             .to_str()
             .unwrap();
         format!("CAST({value} AS {type_be})")
+    }
+
+    #[pg_extern]
+    pub fn check_function(expr: &str) -> bool {
+        crate::pa_check_function(expr)
     }
 
     /// Given a function call (e.g. 'anon.fake_city()'), return the namespace
