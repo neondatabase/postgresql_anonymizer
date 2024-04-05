57a8646cb844c6a8ddfadc7f35c2acbd87bc051a
Thibaut MADELAINE <thibaut.madelaine@gitguardian.com>
2024-04-05 11:29:25 +0200

    No error in stop_dynamic_masking() in case the schema do not exists

--- anon.sql
+++ anon.sql
@@ -2642,7 +2642,7 @@ BEGIN
   WHERE anon.hasmask(oid::REGROLE);
 
   -- Drop the masking schema, it should be empty
-  EXECUTE format('DROP SCHEMA %I',
+  EXECUTE format('DROP SCHEMA IF EXISTS %I',
                   pg_catalog.current_setting('anon.maskschema')
   );
 
