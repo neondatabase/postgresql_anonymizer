4acc9cf04dd6902953f3649364182f73c6561cb9
damien clochard <damien@dalibo.com>
2024-06-10 11:45:42 +0200

    [CI] fix how-to generation

--- docs/how-to/init_postgres.sh
+++ docs/how-to/init_postgres.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+# This works on Ubuntu 24.04 Noble
 
 apt-get update
 
 apt-get install -y --no-install-recommends \
   make gcc \
-  postgresql postgresql-server-dev-14 \
+  postgresql postgresql-server-dev-16 \
   pgxnclient
 
 pgxn install postgresql_anonymizer
@@ -16,6 +17,6 @@ cp pg_hba.conf /tmp
 
 pg_ctlcluster \
   -o "-c client_encoding='UTF8' -c shared_preload_libraries='anon' -c hba_file=/tmp/pg_hba.conf" \
-  14 main restart
+  16 main restart
 
 pg_lsclusters
