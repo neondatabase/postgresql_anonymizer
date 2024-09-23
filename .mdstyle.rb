e0083d0d9d5862297ef0e3705221f2e09a19960d
damien clochard <damien@dalibo.com>
2024-09-23 17:06:48 +0200

    [docs] allow long lines in markdown tables

--- .mdstyle.rb
+++ .mdstyle.rb
@@ -2,6 +2,7 @@ all
 rule 'MD003', :style => :setext_with_atx
 exclude_rule 'MD004'
 exclude_rule 'MD012'
+rule "MD013", :tables => false
 rule "MD024", :allow_different_nesting => true
 rule 'MD025', :level => 2
 exclude_rule 'MD026'
